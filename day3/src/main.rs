7cad689b48e1139760f80f1a47055d9688a83bcb
tijs <tijs@tayami.net>
2025-12-03 10:59:22 +0100

    simplify alternative impl by not copying input at all

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -32,32 +32,34 @@ fn max_j(bank: &[u8], n_digits: usize) -> u64 {
     outcome
 }
 
-// this is O(bank_len * n_digits)
-fn max_j_2(bank: &[u8], n_digits: usize) -> u64 {
-    // state[i] = n -> the maximum outcome so far for i digits is n
-    let mut states = vec![0u64; n_digits + 1];
-    for &c in bank {
-        // reverse otherwise we're reading back newly calculated values
-        for i in (1..=n_digits).rev() {
-            // a state of len i may be formed by appending to state of len (i-1)
-            let shorter = states[i - 1];
-            let nw = shorter * 10 + c as u64;
-            // replace if better than known
-            let existing = &mut states[i];
-            if nw > *existing {
-                *existing = nw;
-            }
-        }
-    }
-    states[n_digits]
-}
-
 fn run(inp: &str, n_digits: usize) -> u64 {
     parse(inp).map(|bank| max_j(&bank, n_digits)).sum()
 }
 
 fn run_2(inp: &str, n_digits: usize) -> u64 {
-    parse(inp).map(|bank| max_j_2(&bank, n_digits)).sum()
+    // state[i] = n -> the maximum outcome so far for i digits is n
+    let mut states = vec![0u64; n_digits + 1];
+    inp.split('\n')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            states.fill(0);
+            for &c in s.as_bytes() {
+                let c = c - b'0';
+                // reverse otherwise we're reading back newly calculated values
+                for i in (1..=n_digits).rev() {
+                    // a state of len i may be formed by appending to state of len (i-1)
+                    let nw = states[i - 1] * 10 + c as u64;
+                    // replace if better than known
+                    let tgt = &mut states[i];
+                    if nw > *tgt {
+                        *tgt = nw;
+                    }
+                }
+            }
+            states[n_digits]
+        })
+        .sum()
 }
 
 #[test]
