192c11ff4c456c3e4018804f5ab336466ad377be
tijs <tijs@tayami.net>
2025-12-08 09:33:26 +0100

    day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::{BinaryHeap, HashMap};
 fn main() {
     let inp = std::io::read_to_string(std::io::stdin()).unwrap();
     println!("answer: {:?}", run(&inp, 1000));
+    println!("answer 2: {:?}", run2(&inp));
 }
 
 type Coord = [u64; 3];
@@ -50,7 +51,6 @@ fn find_connections(
 }
 
 fn run(inp: &str, n_conns: usize) -> usize {
-    // (circuit ID, coord)
     let circuits = parse(inp).collect::<Vec<_>>();
     let conns = find_connections(circuits.iter().copied(), n_conns);
     let mut set_ids = (0..circuits.len()).collect::<Vec<_>>();
@@ -73,6 +73,39 @@ fn run(inp: &str, n_conns: usize) -> usize {
     let mut set_sizes = set_id_to_size.into_values().collect::<Vec<_>>();
     set_sizes.sort_unstable();
     set_sizes.iter().copied().rev().take(3).product::<usize>()
+}
+
+fn run2(inp: &str) -> u64 {
+    let circuits = parse(inp).collect::<Vec<_>>();
+    let mut dists = circuits
+        .iter()
+        .enumerate()
+        .flat_map(|(left_id, &left)| {
+            circuits
+                .iter()
+                .enumerate()
+                .skip(left_id + 1)
+                .map(move |(right_id, &right)| (distance(left, right), left_id, right_id))
+        })
+        .collect::<Vec<_>>();
+    dists.sort_unstable();
+    let mut set_ids = (0..circuits.len()).collect::<Vec<_>>();
+    for (_, left, right) in dists {
+        let left_set = set_ids[left];
+        let right_set = set_ids[right];
+        let mut single_set = true;
+        for s in &mut set_ids {
+            if *s == right_set {
+                *s = left_set;
+            } else if *s != left_set {
+                single_set = false;
+            }
+        }
+        if single_set {
+            return circuits[left][0] * circuits[right][0];
+        }
+    }
+    unreachable!()
 }
 
 #[test]
@@ -100,4 +133,5 @@ fn example() {
         425,690,689
     ";
     assert_eq!(run(inp, 10), 40);
+    assert_eq!(run2(inp), 25272);
 }
