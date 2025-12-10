4341eb29cb9c61cb17506168b68f1efb62608333
tijs <tijs@tayami.net>
2025-12-10 12:06:03 +0100

    day 10 part 2, pre-solve before searching, still untractable

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::cmp::Ordering;
 
 fn main() {
     let inp = std::io::read_to_string(std::io::stdin()).unwrap();
@@ -70,20 +71,117 @@ fn run(inp: &str) -> u32 {
     parse(inp).map(|machine| min_toggles(&machine)).sum()
 }
 
-use std::cmp::Ordering;
+fn min_presses(machine: &Machine) -> u64 {
+    println!("searching {machine:?}");
+    let mut joltage_affected = machine
+        .joltage
+        .iter()
+        .enumerate()
+        .map(|(i, &j)| {
+            let mask = 1 << i;
+            let buttons = machine
+                .buttons
+                .iter()
+                .enumerate()
+                .filter(|&(_, &b)| b & mask != 0)
+                .map(|(j, _)| j)
+                .collect::<Vec<_>>();
+            (j, buttons)
+        })
+        .collect::<Vec<_>>();
+    // it feels prudent to start searching with the joltage that has the lowest
+    // number of button combinations
+    joltage_affected.sort_unstable_by_key(|(_, buttons)| buttons.len());
+    // let's optimise the search range -- e.g. if some joltage is affected only by one button,
+    // then we already know the exact number of presses, and doing this recursively can severely
+    // limit the search.
+    let mut button_ranges = vec![(0u64, u64::MAX); machine.buttons.len()]; // inclusive
+    loop {
+        let mut updated = false;
+        for &(tgt_j, ref buttons) in &joltage_affected {
+            for &b in buttons {
+                let (min_ex, max_ex) = buttons
+                    .iter()
+                    .copied()
+                    .filter(|&other_button| other_button != b)
+                    .map(|ndx| button_ranges[ndx])
+                    .fold((0u64, 0u64), |(min, max), (bmin, bmax)| {
+                        (min + bmin, max.saturating_add(bmax))
+                    });
+                let (current_min, current_max) = button_ranges[b];
+                // if the other buttons add up to at least x, then we can't press this button more than
+                // tgt_j - x
+                let this_max = tgt_j.checked_sub(min_ex).unwrap();
+                if this_max < current_max {
+                    button_ranges[b].1 = this_max;
+                    updated = true;
+                }
+                // if the other buttons add up to at most x, then we must press this button at least tgt_j - x
+                let this_min = tgt_j.saturating_sub(max_ex);
+                if this_min > current_min {
+                    button_ranges[b].0 = this_min;
+                    updated = true;
+                }
+            }
+        }
+        if !updated {
+            break;
+        }
+    }
+    println!("ranges optimised: {button_ranges:?}");
+    let mut presses = button_ranges
+        .iter()
+        .map(|&(min, _)| min)
+        .collect::<Vec<_>>();
+    let mut min_presses = u64::MAX;
+    search(
+        &joltage_affected,
+        &button_ranges,
+        &mut presses,
+        0,
+        &mut min_presses,
+    );
+    min_presses
+}
 
-fn presses_check(counts: &[u64], machine: &Machine) -> Ordering {
+fn search(
+    joltage_affected: &[(u64, Vec<usize>)],
+    ranges: &[(u64, u64)],
+    presses: &mut [u64],
+    i: usize,
+    min: &mut u64,
+) {
+    if i >= presses.len() {
+        return;
+    }
+    let (min_p, max_p) = ranges[i];
+    debug_assert_eq!(presses[i], min_p);
+    for c in min_p..=max_p {
+        presses[i] = c;
+        let total = presses.iter().copied().sum();
+        if total >= *min {
+            break;
+        }
+        match presses_result(joltage_affected, presses) {
+            Ordering::Greater => break,
+            Ordering::Less => search(joltage_affected, ranges, presses, i + 1, min),
+            Ordering::Equal => {
+                *min = total;
+                break;
+            }
+        }
+    }
+    presses[i] = min_p;
+}
+
+fn presses_result(joltage_affected: &[(u64, Vec<usize>)], presses: &[u64]) -> Ordering {
     let mut all_eq = true;
-    for (i, &tgt_j) in machine.joltage.iter().enumerate() {
-        let mask = 1 << i;
-        let j: u64 = std::iter::zip(counts, &machine.buttons)
-            .filter(|&(_, b)| b & mask != 0)
-            .map(|(c, _)| c)
-            .sum();
-        if j > tgt_j {
+    for &(tgt_j, ref buttons) in joltage_affected {
+        let sum: u64 = buttons.iter().map(|&b| presses[b]).sum();
+        if sum > tgt_j {
             return Ordering::Greater;
         }
-        if j < tgt_j {
+        if sum != tgt_j {
             all_eq = false;
         }
     }
@@ -92,40 +190,6 @@ fn presses_check(counts: &[u64], machine: &Machine) -> Ordering {
     } else {
         Ordering::Less
     }
-}
-
-fn search_presses(machine: &Machine, counts: &mut [u64], i: usize, min: &mut u64) {
-    if i >= counts.len() {
-        return;
-    }
-    debug_assert_eq!(counts[i], 0);
-    for c in 0.. {
-        counts[i] = c;
-        let presses = counts.iter().copied().sum();
-        if presses >= *min {
-            break;
-        }
-        match presses_check(counts, machine) {
-            Ordering::Less => {
-                search_presses(machine, counts, i + 1, min);
-            }
-            Ordering::Equal => {
-                *min = presses;
-            }
-            Ordering::Greater => {
-                break;
-            }
-        }
-    }
-    counts[i] = 0;
-}
-
-fn min_presses(machine: &Machine) -> u64 {
-    // println!("searching {machine:?}");
-    let mut counts = vec![0; machine.buttons.len()];
-    let mut min = u64::MAX;
-    search_presses(machine, &mut counts, 0, &mut min);
-    min
 }
 
 fn run2(inp: &str) -> u64 {
